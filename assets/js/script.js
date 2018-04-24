724b1fd9c854297341112ca5c995af8c4ea48a2f
rupingchen <38297617+rupingchen@users.noreply.github.com>
2018-04-24 00:48:06 -0700

    adding site under construction (currently at four jquery fxn)

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,17 @@ $(document).ready(function() {
 	$('#intro').css("margin", "25% 58% 20% 7%");
 	// jQuery Function Number 2
 	$("#intro").slideDown(5000);
+	$("quote").css("display","block");
+	$("#quote").animate({width:'toggle'},7000);
+	// jQuery Function Number 3
+	$("#second").hover(function() {
+		// jQuery Function Number 4
+		$(this).html("this site is still under construction");
+		$(this).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+	})
+	$(window).scroll(function() {
+		$("#pointer-rectangle").show();
+
+	})
 
 })
