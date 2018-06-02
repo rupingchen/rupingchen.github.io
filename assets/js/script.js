0d3101641c14470476b9b1b09ce39a870511bee4
rupingchen <38297617+rupingchen@users.noreply.github.com>
2018-06-01 18:15:54 -0700

    landing page
    completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,28 @@
-$(document).ready(function() {
-	// jQuery Function Number 1
-	$('#intro').css("margin", "25% 58% 20% 7%");
-	// jQuery Function Number 2
-	$("#intro").slideDown(5000);
-	$("quote").css("display","block");
-	$("#quote").animate({width:'toggle'},7000);
-	// jQuery Function Number 3
-	$("#second").hover(function() {
-		// jQuery Function Number 4
-		$(this).html("this site is still under construction");
-		$(this).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-	})
-	// jQuery Function Number 5
-	$("#button1").click(function() {
-		$('html, body').animate({
-			// jQuery Function Number 6
-        scrollTop: $("#what-i-do").offset().top
-    }, 2000);
-	})
+// $(document).ready(function() {
+// 	// jQuery Function Number 1
+// 	$('#intro').css("margin", "25% 58% 20% 7%");
+// 	// jQuery Function Number 2
+// 	$("#intro").slideDown(5000);
+// 	$("quote").css("display","block");
+// 	$("#quote").animate({width:'toggle'},7000);
+// 	// jQuery Function Number 3
+// 	$("#second").hover(function() {
+// 		// jQuery Function Number 4
+// 		$(this).html("this site is still under construction");
+// 		$(this).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+// 	})
+// 	// jQuery Function Number 5
+// 	$("#button1").click(function() {
+// 		$('html, body').animate({
+// 			// jQuery Function Number 6
+//         scrollTop: $("#what-i-do").offset().top
+//     }, 2000);
+// 	})
 
-	// TypeIt Introduction
-	new TypeIt('#intro p', {
-		speed:80,
-		startDelay:6000,
-	}).type('I’m a senior at UC Berkeley studying Statistics, currently purusing an interest in design and furthering my passions in creative writing.').break().break().type('To me, writing is a form of artistry that allows me to understand that which seems nebulous -- a soul-searching of sorts. I infrequently write for the Daily Cal and occasionally update this blog.').break().break().type('Feel free to explore this website and should you feel the need, contact me!')
+// 	// TypeIt Introduction
+// 	new TypeIt('#intro p', {
+// 		speed:80,
+// 		startDelay:6000,
+// 	}).type('I’m a senior at UC Berkeley studying Statistics, currently purusing an interest in design and furthering my passions in creative writing.').break().break().type('To me, writing is a form of artistry that allows me to understand that which seems nebulous -- a soul-searching of sorts. I infrequently write for the Daily Cal and occasionally update this blog.').break().break().type('Feel free to explore this website and should you feel the need, contact me!')
 
-})
+// })
