043346b0207f5b4683439e489b51679978bb0f22
rupingchen <38297617+rupingchen@users.noreply.github.com>
2018-08-13 11:13:26 -0700

    color-fill-in
    trying something new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,61 @@
-$(window).on('scroll', function (){
-	let NEXTPageTEXT = Array.from($('.page-text'))
-	for (let nextpage of NEXTPageTEXT){
-		let nextPageText = NEXTPageTEXT[nextpage]
-	}
-	// var TextPos = $('#intro').offset();
-	// $('.page-text').each(function(){
-	// 	if (window.pageYOffset >= 1270){
-	// 		var nextPageText = $('.page-text').next().prevObject[1].innerText;
-	// 		var oldPageText = $('.page-text')[0].innerText;
-	// 		$('.page-text').html(nextPageText);
-	// 	}
-	// 	else {
-	// 		$('.page-text').html(oldPageText);
-	// 	}
-	// 		return;
-	// 	})
-	$(document).ready(function() {
-		$(window).trigger("scroll");
-	})
+/* 
+
+    Hopefully you've read over the article and have a solid graps of 
+    iteration/enumeration in JS.
+
+    You aren't restricted to using jQuery, but you can if you like. 
+    the document is set up for you, but feel free to delete it.
+
+    Assignment: finish the two event handlers so that they fulfill their 
+    respective functionality. 
+
+*/
+
+$(document).ready(function() {
+
+
+    // PART 1
+    $(".base_coloring").click(function() {
+        /*
+            Re-color the div's with class 'q1-re-color' on the page so
+            that each background color of the nested div of the 'q1-re-color' object.
+            You are allowed no more than 3 lines of code with everything contained 
+            in the for loop.
+
+            HINT: Array.from($(class_list)) returns an 
+            iterable of div objects with class 'class_list' 
+
+            HINT: There are several ways to do this, but if you don't
+            use jQuery you should look up things like 'children array' and 'innerHTML'
+        */
+        // YOUR CODE HERE.
+        for (let value of Array.from($(".landing-re-color"))){
+            let hex_val = value.textContent;
+            $(value).attr("style", "background:" + hex_val);
+        }
+    })
+
+
+    
+
+    // PART 2
+    $(".order_coloring").click(function() {
+        /*
+            Re-color the div's with class 'q1-re-color' on the page so 
+            that each bacground color of the object corresponds to 
+            rgb(10, order-on-page * 12, order-on-page * 10)
+            -> ex) the second div on the page should have background color (10, 12, 10)
+            -> you are allowed no more than 3 lines of code with everything contained 
+            in the for loop.
+
+            HINT: Array.from($(class_list)) returns an 
+            iterable of div objects with class 'class_list' 
+        */ 
+        // YOUR CODE HERE.
+        for (let value of Array.from($(".q1-re-color"))){
+            let id = $(value).attr('id');
+            $(value).attr('style','background:rgb(10,' + (id*12) + "," + (id*10) + ")");
+        }
+
+    })
 })
-	// // jQuery Function Number 1
-	// $('#intro').css("margin", "25% 58% 20% 7%");
-	// // jQuery Function Number 2
-	// $("#intro").slideDown(5000);
-	// $("quote").css("display","block");
-	// $("#quote").animate({width:'toggle'},7000);
-	// // jQuery Function Number 3
-	// $("#second").hover(function() {
-	// 	// jQuery Function Number 4
-	// 	$(this).html("this site is still under construction");
-	// 	$(this).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-	// })
-	// // jQuery Function Number 5
-	// $("#button1").click(function() {
-	// 	$('html, body').animate({
-	// 		// jQuery Function Number 6
- //        scrollTop: $("#what-i-do").offset().top
- //    }, 2000);
-	// })
-
-	// // TypeIt Introduction
-	// new TypeIt('#intro p', {
-	// 	speed:80,
-	// 	startDelay:6000,
-	// }).type('I’m a senior at UC Berkeley studying Statistics, currently purusing an interest in design and furthering my passions in creative writing.').break().break().type('To me, writing is a form of artistry that allows me to understand that which seems nebulous -- a soul-searching of sorts. I infrequently write for the Daily Cal and occasionally update this blog.').break().break().type('Feel free to explore this website and should you feel the need, contact me!')
-
