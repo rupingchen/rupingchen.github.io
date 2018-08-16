363aabd6da3096cb66af8f1f564431b7f526ddc3
rupingchen <38297617+rupingchen@users.noreply.github.com>
2018-08-16 01:34:08 -0700

    text on re-colored boxes
    set class name for text in different sections and change jquery accordingly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
     $(".design_coloring").click(function() {
-        for (let value of Array.from($(".landing-re-color"))){
+        for (let value of Array.from($(".landing-re-color .code"))){
             let hex_val = value.textContent;
-            $(value).attr("style", "background:" + hex_val);
+            $(value).parent().attr("style", "background:" + hex_val);
+        }
+        for (let text of Array.from($(".landing-re-text"))){
+            let text_content = text.textContent;
+            $(text).html(text_content)
         }
     })
     $(".writing_coloring").click(function() {
             for (let value of Array.from($(".landing-re-color"))){
                 let id = $(value).attr('id');
                 $(value).attr('style','background:rgb(185,'+ (id*95) + "," + (id*150) + ")");
+            }
+            for (let text of Array.from($(".landing-re-text"))){
+                let text_content = "Daily Californian" + "TutorMing" + "mildlysatiricalconfessions" + "misc."
+                $(text).html(text_content)
             }
     })
     $(".about_coloring").click(function() {
