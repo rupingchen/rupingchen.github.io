b904595284300cb14cd4f2bf8fcf543039f9fcb8
rupingchen <38297617+rupingchen@users.noreply.github.com>
2018-08-16 20:02:48 -0700

    text on re-colored boxes
    finally figured it out
    changed css via jquery instead of trying to write ANOTHER fxn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
     $(".design_coloring").click(function() {
+        $(".writing-re-text").attr("style", "display:none");
+        $(".design-re-text").attr("style", "display:inline")
         for (let value of Array.from($(".landing-re-color .code"))){
             let hex_val = value.textContent;
             $(value).parent().attr("style", "background:" + hex_val);
         }
-        for (let text of Array.from($(".landing-re-text"))){
-            let text_content = text.firstElementChild.textContent;
+        for (let text of Array.from($(".design-re-text"))){
+            let text_content = text.textContent;
             $(text).html(text_content);
         }
     })
     $(".writing_coloring").click(function() {
+            $(".design-re-text").attr("style", "display:none");
+            $(".writing-re-text").attr("style", "display:inline")
             for (let value of Array.from($(".landing-re-color"))){
                 let id = $(value).attr('id');
                 $(value).attr('style','background:rgb(185,'+ (id*95) + "," + (id*150) + ")");
             }
-            for (let text of Array.from($(".landing-re-text .writing-text"))){
+            for (let text of Array.from($(".writing-re-text"))){
                 let text_content = text.textContent;
                 $(text).html(text_content);
             }
