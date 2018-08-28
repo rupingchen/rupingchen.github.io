2fec42739f22c1ee3bfd5daeb8ac3bb25aae4e46
rupingchen <38297617+rupingchen@users.noreply.github.com>
2018-08-27 17:50:06 -0700

    recolored boxes
    managed to get appropriate images to show up in appropriate boxes :D

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
-    for (let value of Array.from($(".design-img-src"))){
-            let img_src = value.textContent;
-            $("#background-image").attr('style', "background-image: url('" + img_src + "')");
-        }
+    for (let box of Array.from($(".landing-re-color"))){
+        let img_src = box.children[2].textContent;
+        // console.log(img_src)
+        $(box).attr('style', "background-image: url('" + img_src + "')");
+    }
+    // var img_src_test = Array.from($(".design-img-src"));
+    // let img_src_t0 = img_src_test[0].textContent;
+    // let img_src_t1 = img_src_test[1].textContent;
+    // $("background-image-first").attr('style', "background-image: url('" + img_src_t0 + "')");
+    // $("#background-image-second").attr('style', "background-image: url('" + img_src_t1 + "')");
     $(".design_coloring").click(function() {
         $(".writing-re-text").attr("style", "display:none");
         $(".about-re-text").attr("style", "display:none");
         $(".design-re-text").attr("style", "display:inline");
-        for (let value of Array.from($(".design-img-src"))){
-            let img_src = value.textContent;
-            $("#background-image").attr('style', "background-image: url('" + img_src + "')");
+        for (let box of Array.from($(".landing-re-color"))){
+            let img_src = box.children[2].textContent;
+            $(box).attr('style', "background-image: url('" + img_src + "')");
         }
         for (let text of Array.from($(".design-re-text .box-header"))){
             let text_content = text.textContent;
@@ -20,9 +26,9 @@ $(document).ready(function() {
             $(".design-re-text").attr("style", "display:none");
             $(".about-re-text").attr("style", "display:none");
             $(".writing-re-text").attr("style", "display:inline");
-            for (let value of Array.from($(".writing-img-src"))){
-                let img_src = value.textContent;
-                $("#background-image").attr('style', "background-image: url('" + img_src + "')");
+            for (let box of Array.from($(".landing-re-color"))){
+                let img_src = box.children[4].textContent;
+                $(box).attr('style', "background-image: url('" + img_src + "')");
             }
             for (let text of Array.from($(".writing-re-text .box-header"))){
                 let text_content = text.textContent;
